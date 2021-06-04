4fcd911c825395cc8ad57dec0f99c41991f94d8b
Livingstone <livingstonenjaria4@gmail.com>
2021-06-04 17:05:35 +0300

    lesson-03

--- app/src/main/java/com/codestructgroup/swoosh/MainActivity.kt
+++ app/src/main/java/com/codestructgroup/swoosh/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.codestructgroup.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codestructgroup.swoosh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        binding.getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
+        setContentView(view)
+
     }
 }
