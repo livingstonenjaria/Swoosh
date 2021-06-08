b4f263a90d741b02dcb31c821a51d02459670705
Livingstone <livingstonenjaria4@gmail.com>
2021-06-09 02:57:37 +0300

    lesson-05

--- app/src/main/java/com/codestructgroup/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/codestructgroup/swoosh/LeagueActivity.kt
@@ -1,11 +1,20 @@
 package com.codestructgroup.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codestructgroup.swoosh.databinding.ActivityLeagueBinding
 
-class LeagueActivity : BaseActivity() {
+class LeagueActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLeagueBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league)
+        binding = ActivityLeagueBinding.inflate(layoutInflater)
+        val view = binding.root
+        binding.leagueNextBtn.setOnClickListener {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            startActivity(skillIntent)
+        }
+        setContentView(view)
     }
 }
--- app/src/main/java/com/codestructgroup/swoosh/MainActivity.kt
+++ app/src/main/java/com/codestructgroup/swoosh/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codestructgroup.swoosh.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
